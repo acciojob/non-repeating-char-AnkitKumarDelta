6de43934947ce1d6f9b9265f6100f7b94be92cbd
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-08-06 19:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let m = new Map();
+for(let i of input){
+	m.set(i,(m.get(i)||0)+1);
+}
+for(let [k,v] of m){
+	if(v==1)return k;
+}
+return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
+
